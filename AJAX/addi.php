9ebc0ca869254dd88ba138269a2f24c267cb3710
alifuk <amdalifuk@gmail.com>
2015-05-11 18:26:58 +0200

    parsování faviconů

--- AJAX/addi.php
+++ AJAX/addi.php
@@ -6,13 +6,13 @@ require_once '../connect.php';
 
 //echo $_POST['link'] . $_POST['owner'] . $_POST['tags'];
 
-if (isset($_POST['link']) && isset($_POST['owner']) && $_POST['link'] != "" && $_POST['owner'] != "" ) {
+if (isset($_POST['link']) && isset($_POST['owner']) && $_POST['link'] != "" && $_POST['owner'] != "") {
 
-    
+
     $stmt = $conn->prepare('INSERT INTO links (link, owner, name, image)
 VALUES (?,?,?,?)');
     $stmt->bind_param('siss', $link, $owner, $title, $image);
-    
+
     $link = $_POST['link'];
     $owner = $_POST['owner'];
     $stmt->execute();
@@ -22,34 +22,35 @@ VALUES (?,?,?,?)');
     $tagy = explode(",", $_POST['tags']);
 
     foreach ($tagy as $tag) {
-
-        $stmt = $conn->prepare('SELECT Id FROM tags WHERE name = ? LIMIT 1');
-        $stmt->bind_param('s', $tag);
-
-        $stmt->execute();
-        $tagId = 0;
-        $stmt->bind_result($tagId);
-
-        $existuje = false;
-
-        while ($stmt->fetch()) {
-            $existuje = true;
-        }
-
-
-        if (!$existuje) {
-
-            $stmt = $conn->prepare('INSERT INTO tags (name) VALUES (?)');
+        if ($tag != "") {
+            $stmt = $conn->prepare('SELECT Id FROM tags WHERE name = ? LIMIT 1');
             $stmt->bind_param('s', $tag);
 
             $stmt->execute();
-            $tagId = $stmt->insert_id;
+            $tagId = 0;
+            $stmt->bind_result($tagId);
+
+            $existuje = false;
+
+            while ($stmt->fetch()) {
+                $existuje = true;
+            }
+
+
+            if (!$existuje) {
+
+                $stmt = $conn->prepare('INSERT INTO tags (name) VALUES (?)');
+                $stmt->bind_param('s', $tag);
+
+                $stmt->execute();
+                $tagId = $stmt->insert_id;
+            }
+
+            $stmt = $conn->prepare('INSERT INTO linkscontags (linkId, tagId) VALUES (?,?)');
+            $stmt->bind_param('ii', $linkId, $tagId);
+
+            $stmt->execute();
         }
-
-        $stmt = $conn->prepare('INSERT INTO linkscontags (linkId, tagId) VALUES (?,?)');
-        $stmt->bind_param('ii', $linkId, $tagId);
-
-        $stmt->execute();
     }
 
 
--- AJAX/linkParser.php
+++ AJAX/linkParser.php
@@ -2,26 +2,64 @@
 
 if (isset($_POST['link']) && $_POST['link'] != "" && isset($_POST['owner']) && $_POST['owner'] != "") {
 
-    
+
     include('../simplehtmldom_1_5/simple_html_dom.php');
-    
+
     $html = file_get_html($_POST['link']);
     $title = "";
     $image = "";
-    foreach($html->find('title') as $e){
-        $title =  $e->innertext;        
+    foreach ($html->find('title') as $e) {
+        $title = $e->innertext;
+    }
+
+    foreach ($html->find('meta[property="og:image"]') as $e) {
+        $image = $e->content;
     }
     
-    foreach($html->find('meta[property="og:image"]') as $e){
-        $image =  $e->content;        
+    if (trim($image) == "") {
+        foreach ($html->find('link[rel="shortcut icon"]') as $e) {
+            $image = $e->href;
+            echo "juuuuu";
+            if(strrpos($image, "/") == 0){
+                
+                $texkaPos = strrpos($_POST['link'], ".",-1);
+                $adresaEnd = 0;
+                if(strpos($_POST['link'], "/",$texkaPos) == -1){
+                    $image = $_POST['link'].$image;
+                } else{
+                    echo $texkaPos . "  ";
+                    echo substr($_POST['link'], 0, strpos($_POST['link'], "/",$texkaPos));
+                    $image = substr($_POST['link'], 0, strpos($_POST['link'], "/",$texkaPos)) .$image ;
+                }
+                
+            }
+        }
     }
     
-    //echo $title;
+    if (trim($image) == "") {
+        foreach ($html->find('link[rel="icon"]') as $e) {
+            
+            $image = $e->href;
+            echo "juuuuu";
+            if(strrpos($image, "/") == 0){
+                
+                $texkaPos = strrpos($_POST['link'], ".",-1);
+                $adresaEnd = 0;
+                if(strpos($_POST['link'], "/",$texkaPos) == -1){
+                    $image = $_POST['link'].$image;
+                } else{
+                    echo $texkaPos . "  ";
+                    echo substr($_POST['link'], 0, strpos($_POST['link'], "/",$texkaPos));
+                    $image = substr($_POST['link'], 0, strpos($_POST['link'], "/",$texkaPos)) .$image ;
+                }
+                
+            }
+            
+            
+        }
+    }
+    echo $image;
     include 'addi.php';
-    
-    
-    
-    
 }
 ?>
 
